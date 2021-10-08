6c69a1052c2293c29a143a3d4be61927bcce6f2e
kato9292929 <76525458+kato9292929@users.noreply.github.com>
2021-10-09 02:11:12 +0900

    11 11

--- src/index.js
+++ src/index.js
@@ -15,12 +15,11 @@ const onClickAdd = () => {
 
   // button（完了）タグ生成
   const completeButton = document.createElement("button");
-  completeButton.innerTextt = "完了";
+  completeButton.innerText = "完了";
 
   // button（削除）タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
-  console.log(deleteButton);
 
   // divタグの子要素に各要素を設定
   div.appendChild(li);
